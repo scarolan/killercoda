41fcd65998f04f9634c504fe24746026b6d4d42d
Sean Carolan <sean.carolan@grafana.com>
2025-06-12 15:35:08 -0400

    Try again to enable fancy prompt.

--- grafana-alloy-linux/background.sh
+++ grafana-alloy-linux/background.sh
@@ -7,6 +7,14 @@ wget -q -O - https://apt.grafana.com/gpg.key | gpg --dearmor | tee /etc/apt/keyr
 echo "deb [signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com stable main" | tee /etc/apt/sources.list.d/grafana.list
 apt -y update
 
+# Download Grafana Alloy VSIX to current directory
+VSIX_URL="https://github.com/grafana/vscode-alloy/releases/download/v0.2.0/grafana-alloy-0.2.0.vsix"
+VSIX_NAME="grafana-alloy-0.2.0.vsix"
+
+# Download the VSIX file using wget
+wget -q "$VSIX_URL" -O "$VSIX_NAME"
+echo "✅ Downloaded $VSIX_NAME to $(pwd)"
+
 # Enable fancy prompt
 wget -O ~/.fancy-prompt.sh https://raw.githubusercontent.com/scarolan/fancy-linux-prompt/master/fancy-prompt.sh
 cat <<'EOF' >> ~/.bashrc
@@ -24,11 +32,9 @@ else
 fi
 EOF
 
-# Download Grafana Alloy VSIX to current directory
-VSIX_URL="https://github.com/grafana/vscode-alloy/releases/download/v0.2.0/grafana-alloy-0.2.0.vsix"
-VSIX_NAME="grafana-alloy-0.2.0.vsix"
-
-# Download the VSIX file using wget
-wget -q "$VSIX_URL" -O "$VSIX_NAME"
-echo "✅ Downloaded $VSIX_NAME to $(pwd)"
-
+# Set enable the fancy prompt
+if [[ "\$TERM" == "xterm-color" ]]; then
+  export PS1="\[\e[1;36m\]\u\[\e[0m\]@\[\e[1;32m\]\h\[\e[0m\]:\[\e[1;34m\]\w\[\e[0m\] \$ "
+else
+  source ~/.fancy-prompt.sh
+fi
--- grafana-alloy-linux/foreground.sh
+++ grafana-alloy-linux/foreground.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 set -euo pipefail  # Exit on error, show commands
-sleep 3
 
 # This script runs after the background script completes.
-if [[ "\$TERM" == "xterm-color" ]]; then
-  export PS1="\[\e[1;36m\]\u\[\e[0m\]@\[\e[1;32m\]\h\[\e[0m\]:\[\e[1;34m\]\w\[\e[0m\] \$ "
-else
-  source ~/.fancy-prompt.sh
-fi
 echo "Hello world!"
