950a1bdef27a08c4f59f1af3e98a7fcdefa1591f
Sean Carolan <sean.carolan@grafana.com>
2025-06-12 16:33:59 -0400

    Adds grafana logo to shell

--- grafana-alloy-linux/foreground.sh
+++ grafana-alloy-linux/foreground.sh
@@ -1,3 +1,33 @@
-#!/bin/bash
-echo "Please hit enter to reload the terminal."
-exit 0
+(
+GRAFANA_LOGO=$(cat <<'EOF'
+                        ltmg
+                       tmmmmtl
+                     lmmmmmmmmg
+                 lggtmmmmmmmmmmmmttggtmmmm
+     gttmmmmmmttmmmmmmmmmmmmmmmmmmmmmmmmmml
+     tmmmmmmmmmmmmmmmm               tmmmmtl
+     lmmmmmmmmmmmmt                      mmmg
+      gmmmmmmmmmt                          tmt
+       lmmmmmmmg           llgggggll         mm
+       tmmmmmmg         gtmmmmmmmmmmmmtl      mg
+       mmmmmmm        gmmmg        tmmmmmg     m
+     ltLLLLLLg       gLLt            gLLLLml
+  ltmLLLLLLLLg       LLL              lLLLLm
+ tLLLLLLLLLLLm       mLL               mLLLLg
+tLLLLLLLLLLLLLl       tLt              LLLLLt
+  lLLLLLLLLLLLLg        lLtglllg      gLLLLLLg
+       gLLLLLLLLtl                   gLLLLLLLLm
+          tLLLLLLLmg               gmLLLLLLLLm
+            mGGGGGGGLmtglll  llggmLGGGGGGl
+            LGGGGGGGGGGGGGGGGGGGGGGGGGG
+           lGGGGGGGGGGGGGGGGGGGGGGGGGg
+            GGGGGGL         gGGGGGGGL
+                               LGGGt
+                                  g
+EOF
+YELLOW='\033[1;33m'
+RESET='\033[0m'
+) && clear
+echo -e "${YELLOW}${GRAFANA_LOGO}${RESET}"
+echo "Please hit enter to continue..."
+
