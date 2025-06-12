862dbaa809a035e8d127fd45a6d26d9dd90c05a9
Sean Carolan <sean.carolan@grafana.com>
2025-06-12 15:38:09 -0400

    Ask the user to restart the terminal.

--- grafana-alloy-linux/foreground.sh
+++ grafana-alloy-linux/foreground.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-set -euo pipefail  # Exit on error, show commands
-
 # This script runs after the background script completes.
-echo "Hello world!"
+echo "Please hit Enter to reload the terminal..."
