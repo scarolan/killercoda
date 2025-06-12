c30c4c1abcc978b5a979697c999f75e52cac84c9
Sean Carolan <sean.carolan@grafana.com>
2025-06-12 15:39:00 -0400

    Adds a sleep to delay loading of terminal env.

--- grafana-alloy-linux/background.sh
+++ grafana-alloy-linux/background.sh
@@ -32,9 +32,4 @@ else
 fi
 EOF
 
-# Set enable the fancy prompt
-if [[ "\$TERM" == "xterm-color" ]]; then
-  export PS1="\[\e[1;36m\]\u\[\e[0m\]@\[\e[1;32m\]\h\[\e[0m\]:\[\e[1;34m\]\w\[\e[0m\] \$ "
-else
-  source ~/.fancy-prompt.sh
-fi
+sleep 5
