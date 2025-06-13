5209ba68cbf02f5e59885569c56062eb975c7c8b
Sean Carolan <sean.carolan@grafana.com>
2025-06-12 21:38:14 -0400

    Run silently.

--- grafana-alloy-linux/foreground.sh
+++ grafana-alloy-linux/foreground.sh
@@ -1,14 +1,12 @@
-GRAFANA_NAME=$(cat <<'EOF'
-  _____            __                    _           _         
- / ____|          / _|                  | |         | |        
-| |  __ _ __ __ _| |_ __ _ _ __   __ _  | |     __ _| |__  ___ 
-| | |_ | '__/ _` |  _/ _` | '_ \ / _` | | |    / _` | '_ \/ __|
-| |__| | | | (_| | || (_| | | | | (_| | | |___| (_| | |_) \__ \
- \_____|_|  \__,_|_| \__,_|_| |_|\__,_| |______\__,_|_.__/|___/
-EOF
-)
+# Run silently, only show final output
+(
+  # Define colors
+  ORANGE=$'\033[38;5;208m'
+  YELLOW=$'\033[1;33m'
+  GREEN=$'\033[38;5;82m'
+  RESET=$'\033[0m'
 
-GRAFANA_LOGO=$(cat <<'EOF'
+  GRAFANA_LOGO=$(cat <<'EOF'
                         ltmg
                        tmmmmtl
                      lmmmmmmmmg
@@ -34,16 +32,22 @@ tLLLLLLLLLLLLLl       tLt              LLLLLt
                                LGGGt
                                   g
 EOF
-)
+  )
 
-YELLOW=$'\033[38;5;208m'
-ORANGE=$'\033[38;5;208m'
-GREEN=$'\033[38;5;82m'
-RESET=$'\033[0m'
+  GRAFANA_NAME=$(cat <<'EOF'
+  _____            __                    _           _         
+ / ____|          / _|                  | |         | |        
+| |  __ _ __ __ _| |_ __ _ _ __   __ _  | |     __ _| |__  ___ 
+| | |_ | '__/ _` |  _/ _` | '_ \ / _` | | |    / _` | '_ \/ __|
+| |__| | | | (_| | || (_| | | | | (_| | | |___| (_| | |_) \__ \
+ \_____|_|  \__,_|_| \__,_|_| |_|\__,_| |______\__,_|_.__/|___/
+EOF
+  )
 
-clear
-echo -e "${YELLOW}${GRAFANA_LOGO}${RESET}"
-echo -e "${YELLOW}${GRAFANA_NAME}${RESET}"
-echo -e "${GREEN}Welcome to your Grafana training environment. Please hit enter to start the lab.${RESET}"
-sleep 1
-exit 0
+  clear
+  echo -e "${YELLOW}${GRAFANA_LOGO}${RESET}"
+  echo -e "${ORANGE}${GRAFANA_NAME}${RESET}"
+  echo -e "${GREEN}Welcome to your Grafana training environment. Please hit enter to start the lab.${RESET}"
+) >/dev/tty
+
+read
