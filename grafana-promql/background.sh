3e64fa322845d87124ff7d653091abfd800374c4
Sean Carolan <sean.carolan@grafana.com>
2025-07-02 12:46:02 -0400

    Adds YAML to the prometheus scenario.

--- grafana-promql/background.sh
+++ grafana-promql/background.sh
@@ -19,6 +19,7 @@ declare -A VSIX_FILES=(
   ["grafana-alloy-0.2.0.vsix"]="https://github.com/grafana/vscode-alloy/releases/download/v0.2.0/grafana-alloy-0.2.0.vsix"
   ["dracula-theme.theme-dracula-2.25.1.vsix"]="https://open-vsx.org/api/dracula-theme/theme-dracula/2.25.1/file/dracula-theme.theme-dracula-2.25.1.vsix"
   ["jdinhlife.gruvbox-1.28.0.vsix"]="https://open-vsx.org/api/jdinhlife/gruvbox/1.28.0/file/jdinhlife.gruvbox-1.28.0.vsix"
+  ["redhat.vscode-yaml-1.9.1.vsix"]="https://open-vsx.org/api/redhat/vscode-yaml/1.9.1/file/redhat.vscode-yaml-1.9.1.vsix"
 )
 
 # Create downloads directory and Theia extensions directory
