660aa6bf1a4b1b55336b0144248a2cae1558391a
Sean Carolan <sean.carolan@grafana.com>
2025-07-16 08:29:53 -0400

    add shellscript syntax.

--- grafana-alloy/background.sh
+++ grafana-alloy/background.sh
@@ -22,6 +22,7 @@ declare -A VSIX_FILES=(
   ["grafana-alloy-0.2.0.vsix"]="https://github.com/grafana/vscode-alloy/releases/download/v0.2.0/grafana-alloy-0.2.0.vsix"
   ["dracula-theme.theme-dracula-2.25.1.vsix"]="https://open-vsx.org/api/dracula-theme/theme-dracula/2.25.1/file/dracula-theme.theme-dracula-2.25.1.vsix"
   ["jdinhlife.gruvbox-1.28.0.vsix"]="https://open-vsx.org/api/jdinhlife/gruvbox/1.28.0/file/jdinhlife.gruvbox-1.28.0.vsix"
+  []["jeff-hykin.better-shellscript-syntax-1.10.0.vsix"]="https://open-vsx.org/api/jeff-hykin/better-shellscript-syntax/1.10.0/file/jeff-hykin.better-shellscript-syntax-1.10.0.vsix"
 )
 
 # Download each VSIX file
